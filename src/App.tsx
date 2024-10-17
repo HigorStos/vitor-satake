a50c1cf356b00c783edc3fcca6888af5dd33a12e
Higor Santos <higor.santos@modalgr.io>
2024-10-17 16:43:09 -0300

    feat: meus projetos

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Container from './components/Container';
 import WorkExperience from './components/WorkExperience';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Hero />
         <About />
         <WorkExperience />
+        <Projects />
       </Container>
     </>
   );
---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,58 @@
+import terraformImage from '../../assets/terraform.png';
+
+const Projects = () => {
+  return (
+    <section className='w-full mt-14 flex flex-col justify-center items-center'>
+      <h1 className='font-bold text-2xl'>Meus Projetos</h1>
+      <div className='grid grid-cols-3 gap-14 mt-8'>
+        <div className='max-w-sm rounded overflow-hidden border border-gray-200 shadow-lg bg-white pb-4'>
+          <img className='w-full' src={terraformImage} alt='Terraform' />
+          <div className='px-6 py-4'>
+            <div className='font-bold text-xl mb-2'>Terraform + AWS</div>
+            <p className='text-gray-700 text-base'>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+          <div className='px-6 pt-4 pb-2 flex items-center justify-center'>
+            <button className='bg-orange-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300'>
+              Visualizar projeto
+            </button>
+          </div>
+        </div>
+        <div className='max-w-sm rounded overflow-hidden border border-gray-200 shadow-lg bg-white pb-4'>
+          <img className='w-full' src={terraformImage} alt='Terraform' />
+          <div className='px-6 py-4'>
+            <div className='font-bold text-xl mb-2'>Terraform + AWS</div>
+            <p className='text-gray-700 text-base'>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+          <div className='px-6 pt-4 pb-2 flex items-center justify-center'>
+            <button className='bg-orange-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300'>
+              Visualizar projeto
+            </button>
+          </div>
+        </div>
+        <div className='max-w-sm rounded overflow-hidden border border-gray-200 shadow-lg bg-white pb-4'>
+          <img className='w-full' src={terraformImage} alt='Terraform' />
+          <div className='px-6 py-4'>
+            <div className='font-bold text-xl mb-2'>Terraform + AWS</div>
+            <p className='text-gray-700 text-base'>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+          <div className='px-6 pt-4 pb-2 flex items-center justify-center'>
+            <button className='bg-orange-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300'>
+              Visualizar projeto
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
